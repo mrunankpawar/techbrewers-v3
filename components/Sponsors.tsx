fd6e9fb3bfbc8389b07fd0a9fc127644d805b637
Arbaj9307 <begadearbaj@gmail.com>
2024-08-30 10:24:03 +0530

    STYLE: Sponsers
    Increased font size heading for smaller screen size

--- components/Sponsors.tsx
+++ components/Sponsors.tsx
@@ -12,7 +12,7 @@ const sponsors = [
     logo: '/images/sponsors/securityium.png',
     url: 'https://www.securityium.com/',
   },
-  
+
   {
     name: 'Daftar',
     logo: '/images/sponsors/daftar.png',
@@ -38,7 +38,7 @@ const sponsors = [
     logo: '/images/sponsors/gmc.png',
     url: 'https://givemycertificate.com/',
   },
-  
+
   {
     name: 'Hack This Fall',
     logo: '/images/sponsors/htf.png',
@@ -64,7 +64,7 @@ const sponsors = [
     logo: '/images/sponsors/devweek.png',
     url: 'https://www.developerweek.com/',
   },
-  
+
   {
     name: 'Aeka Coffee',
     logo: '/images/sponsors/aeka.png',
@@ -95,7 +95,7 @@ const sponsors = [
     logo: '/images/sponsors/gdg.jpg',
     url: 'https://gdg.community.dev/gdg-silicon-valley/',
   },
-  
+
 
 ];
 
@@ -104,30 +104,31 @@ function Sponsors() {
   return (
     <div className="mt-0 md:mt-10 h-auto w-full rounded-md flex flex-col items-center justify-center relative overflow-hidden mx-auto py-5 md:py-10">
       <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center text-center">
-        <h3 className="text-xl md:text-3xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-violet-50 to-violet-400 z-10">
+        <h3 className="text-2xl sm:text-3xl md:text-4xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-violet-50 to-violet-400 z-10">
           Sponsors and Partners
         </h3>
+
         <div className="container mx-auto px-4 mt-10">
-        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-4">
-          {sponsors.map((sponsor, index) => (
-            <div key={index} 
-            className="flex items-center justify-center p-4 border border-violet-200 rounded-lg shadow-md hover:shadow-lg filter transition duration-300">
-              <Link href={sponsor.url} passHref target="__blank">
-                  
-                    <Image
-                      src={sponsor.logo}
-                      alt={sponsor.name}
-                      width={150}
-                      height={150}
-                      className="object-contain"
-                    />
+          <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-4">
+            {sponsors.map((sponsor, index) => (
+              <div key={index}
+                className="flex items-center justify-center p-4 border border-violet-200 rounded-lg shadow-md hover:shadow-lg filter transition duration-300">
+                <Link href={sponsor.url} passHref target="__blank">
+
+                  <Image
+                    src={sponsor.logo}
+                    alt={sponsor.name}
+                    width={150}
+                    height={150}
+                    className="object-contain"
+                  />
                 </Link>
-            </div>
-          ))}
+              </div>
+            ))}
+          </div>
         </div>
       </div>
-      </div>
-      
+
     </div>
   )
 }
