eddaa154b3854c93dfce335a4e029423db3044e3
Arbaj9307 <138779256+arbajbegade@users.noreply.github.com>
2024-09-01 12:03:59 +0530

    STYLE: Events
    Added Padding on top.

--- components/Events.tsx
+++ components/Events.tsx
@@ -4,7 +4,7 @@ import { EvervaultCard } from "./ui/EvervaultCard";
 
 function Events() {
   return (
-    <div className="mt-0 md:mt-10 h-auto w-full rounded-md flex flex-col items-center justify-center relative overflow-hidden mx-auto md:py-20">
+    <div className="mt-0 pt-32 md:mt-10 h-auto w-full rounded-md flex flex-col items-center justify-center relative overflow-hidden mx-auto md:py-20">
       <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center text-center">
         <h3 className="mb-2 text-2xl sm:text-3xl md:text-4xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-violet-50 to-violet-400 z-10">
           Upcoming Events
