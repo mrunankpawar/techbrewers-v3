99585e1ebe44290eab1e1ed9d025fa75211a5085
Arbaj9307 <138779256+arbajbegade@users.noreply.github.com>
2024-09-06 16:49:30 +0530

    STYLE: Hero
    Added padding above the button
    also reduces the size of logo for small screen

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -10,21 +10,19 @@ function Hero() {
   return (
     <div className="relative h-screen w-full pb-20">
       <div className="relative flex flex-col items-center justify-center mx-auto h-full w-full max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] text-center">
-        <Image
+        <img
           src="/images/techbrewers.png"
           alt="TechBrewers logo"
-          width={190}
-          height={190}
-          className="pb-5"
+          className="pb-5 w-40 md:w-44 lg:w-48"
         />
         <h2 className="mb-3 text-lg sm:text-xl md:text-2xl lg:text-2xl font-medium sm:font-semibold md:font-bold lg:font-extrabold text-blue-100 uppercase tracking-widest z-10">
           <FlipWords words={words} /> with
         </h2>
 
-        <h1 className="mt-5 md:mt-0 text-4xl md:text-7xl font-bold text-transparent bg-clip-text bg-gradient-to-b from-violet-50 to-violet-400 z-10">
+        <h1 className="mt-5 md:mt-0 text-5xl md:text-7xl font-bold text-transparent bg-clip-text bg-gradient-to-b from-violet-50 to-violet-400 z-10">
           Club TechBrewers
         </h1>
-        <div className="mt-4 z-10">
+        <div className="mt-8 lg:mt-4 z-10">
           <Link href="https://bit.ly/TBDISCORD" target="__blank">
             <MagicButton
               title="Join the community"
