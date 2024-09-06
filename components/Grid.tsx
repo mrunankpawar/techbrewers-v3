36b35df1d3acef5cf95e6ac4d20a78cbbba10426
Mrunank Pawar <65391854+mrunankpawar@users.noreply.github.com>
2024-09-06 00:07:50 -0700

    style: updated copywriting

--- components/Grid.tsx
+++ components/Grid.tsx
@@ -6,10 +6,10 @@ const Grid = () => {
     <div className="mt-0 md:mt-10 h-auto w-full rounded-md flex flex-col items-center justify-center relative overflow-hidden mx-auto py-5 md:py-10">
       <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center text-center">
         <h3 className="text-2xl sm:text-3xl md:text-4xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-violet-50 to-violet-400 z-10">
-          Some numbers that matter
+        Join the Global Tech Network
         </h3>
         <p className="mt-4 font-normal text-base md:text-lg text-violet-200 max-w-xl mx-auto z-10">
-          Connecting a global community of tech enthusiasts, we&apos;re a diverse group driven by shared passion and collaboration.
+        Discover our global network, stay informed with our updates, and join us in empowering tech enthusiasts.
         </p>
       </div>
       <BentoGrid className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] mt-10 lg:mt-12">
