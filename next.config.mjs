f93e2e25cf0154ab701f457b8bbd356fd28a7865
Mrunank Pawar <65391854+mrunankpawar@users.noreply.github.com>
2024-09-08 21:25:53 -0700

    fix: updated config file

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      domains: ['assets.aceternity.com', 'images.unsplash.com'],
-    },
-};
-
-module.exports = {
+  images: {
+    domains: ['assets.aceternity.com', 'images.unsplash.com'],
+  },
   experimental: {
     modern: true,
     polyfillsOptimization: true,
   },
 };
-
 
 export default nextConfig;
