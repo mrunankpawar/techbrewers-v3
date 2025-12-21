7266f4bd7693205749b1ade24e837fd41d0074ec
Mrunank Pawar <65391854+mrunankpawar@users.noreply.github.com>
2025-12-20 19:30:31 -0800

    fixed heading

--- components/Events.tsx
+++ components/Events.tsx
@@ -3,17 +3,17 @@ import React from "react";
 
 function Events() {
   return (
-    <div className="mt-0 md:mt-10 h-auto w-full rounded-md flex flex-col items-center justify-center relative overflow-hidden mx-auto md:py-20 gradient-background">
+    <div className="mt-0 md:mt-10 h-auto w-full rounded-md flex flex-col items-center justify-center relative overflow-hidden mx-auto md:py-20">
       <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center text-center">
-        <h3 className="mb-2 text-4xl md:text-6xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-white to-white/50 z-10">
+        <h2 className="mb-4 text-4xl md:text-6xl font-bold leading-tight bg-clip-text text-transparent bg-gradient-to-b from-white to-white/50 z-10 pb-2">
           Upcoming Events
-        </h3>
-        <h5 className="mt-8 mb-8 md:mt-16 md:mb-16 text-md md:text-lg font-bold text-orange-300/[0.4] z-10">
+        </h2>
+        <p className="mt-8 mb-12 md:mt-12 md:mb-16 text-lg text-gray-300 z-10">
           No upcoming events at the moment
-        </h5>
-        <h3 className="mb-3 text-4xl md:text-6xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-white to-white/50 z-10">
+        </p>
+        <h2 className="mb-3 text-4xl md:text-6xl font-bold leading-tight bg-clip-text text-transparent bg-gradient-to-b from-white to-white/50 z-10 pb-2">
           Past Events
-        </h3>
+        </h2>
       </div>
     </div>
   );
